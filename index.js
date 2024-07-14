0fc9c8c50576f306477ce543be5a51fb4d3e5f8b
Michael Akoko <akokomichael37@gmail.com>
2024-07-14 21:54:33 +0300

    update script

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Wambui", "Otieno", "Kyalo"];
+const event = "Birthday";
+
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+console.log(writeCards(names, event));
+
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown(10);
+
